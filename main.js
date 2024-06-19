d113c6e40e270617386d9714b8e1a9312d3e734f
k-alex-leon <kelemon824@gmail.com>
2024-06-18 20:51:21 -0500

    feat: paddle and game over status

--- main.js
+++ main.js
@@ -24,24 +24,65 @@ const config = {
 };
 const game = new Phaser.Game(config);
 
-let ball = null;
+let ball;
+let paddle;
 // takes care of preload the assets
 function preload() {
   this.load.image("ball", "/ball.png");
+  this.load.image("paddle", "/paddle.png");
 }
 // is executed once when everithing is loaded and ready
 function create() {
-   ball = this.physics.add.image(100, 100, 'ball')
-   ball.setCircle(10)
+  // ball config
+  ball = this.physics.add.image(
+    game.canvas.width * 0.5,
+    game.canvas.height - 20,
+    "ball"
+  );
+  // set a radius
+  ball.setCircle(10);
 
-   ball.setCollideWorldBounds(true)
-   ball.setBounce(1)
+  ball.setCollideWorldBounds(true);
+  ball.setBounce(1);
 
-   ball.setVelocity(150)
+  ball.setVelocity(150, -150);
 
-   this.physics.add.collider(ball)
+  this.physics.add.collider(ball);
+
+  //  this.physics.arcade.checkCollision.down = false;
+
+  // paddle config
+  paddle = this.physics.add.image(
+    game.canvas.width * 0.5,
+    game.canvas.height - 10,
+    "paddle"
+  );
+  // define position
+  paddle.x = game.input.x || game.canvas.width * 0.5;
+  // bouns with borders
+  // paddle.setCollideWorldBounds(true)
+  // doesnt move if it got hit
+  paddle.setImmovable(true);
+  // add a collide body to the image
+  this.physics.add.collider(paddle);
+
+  // no bottom collision
+  this.physics.world.checkCollision.down = false;
+  // this.physics.world.setBoundsCollision(true, true, true, false)
+
   console.log("I'm ready!");
 }
 // is executed on everty frame
 function update() {
+  this.physics.world.collide(ball, paddle);
+  paddle.x = this.input.x;
+
+  if (ball.y > game.canvas.height) {
+    console.log("GAME OVER!");
+    game.destroy(true, false)
+    // game.scene.pause();
+    // this.physics.pause();
+    // game.destroy(true)
+    // location.reload()
+  }
 }
